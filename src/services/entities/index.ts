302082aff3d67ce2cd0f73164c4553e01b8233e5
Alcides Turruellas <ayunierto@gmail.com>
2024-10-25 05:23:38 -0500

    created product-image entity

---
+++ src/services/entities/index.ts
@@ -0,0 +1,2 @@
+export * from './service-image.entity';
+export * from './service.entity';
--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Service } from './entities/service.entity';
+import { Service, ServiceImage } from './entities/';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ServicesController],
   providers: [ServicesService],
-  imports: [TypeOrmModule.forFeature([Service]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Service, ServiceImage]), AuthModule],
 })
 export class ServicesModule {}
