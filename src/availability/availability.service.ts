ba2b97d99641bad46b28c2b3599e525a7c7fc54d
Alcides Turruellas <ayunierto@gmail.com>
2025-01-07 17:07:34 -0500

    feat(availability): logic was implemented so that available slots are not saved if the time has already passed

--- src/availability/availability.service.ts
+++ src/availability/availability.service.ts
@@ -24,11 +24,9 @@ export class AvailabilityService {
   ) {
     // Get the day of the week (1-7) 1: Monday
     const weekday = DateTime.fromISO(date).weekday;
-    console.warn({ weekday });
     const schedule = await this.scheduleRepository.findOne({
       where: { staff: { id: staffId }, weekday },
     });
-    console.warn({ schedule });
 
     if (!schedule) return [];
 
@@ -39,7 +37,6 @@ export class AvailabilityService {
       schedule.startTime,
       userTimeZone,
     );
-    console.warn({ currentStartDateTime });
 
     // Schedules end time (considering user's time zone)
     const scheduleEndDateTime = this.localDateTimeToUTCFormat(
@@ -47,11 +44,11 @@ export class AvailabilityService {
       schedule.endTime,
       userTimeZone,
     );
-    console.warn({ scheduleEndDateTime });
 
     const availableSlots: { start: string; end: string }[] = [];
-    console.warn({ availableSlots });
-    console.warn('=======================');
+
+    // Get the current time in the user's time zone
+    const now = DateTime.now().setZone(userTimeZone);
 
     // Loop through each hour of the schedule
     for (
@@ -59,13 +56,19 @@ export class AvailabilityService {
       currentHour < scheduleEndDateTime.getHours();
       currentHour++
     ) {
-      console.warn({ currentHour });
       const startTime = new Date(currentStartDateTime);
       startTime.setHours(currentHour); // Set time to the beginning of the hour
-      console.warn({ startTime });
       const endTime = new Date(startTime);
       endTime.setHours(startTime.getHours() + 1); // Set time to the end of the hour (next hour)
-      console.warn({ endTime });
+
+      // Convert Starttime to the user's time zone
+      const startTimeUserTz =
+        DateTime.fromJSDate(startTime).setZone(userTimeZone);
+
+      // Jump the iteration if the start time has already passed
+      if (startTimeUserTz < now) {
+        continue; // Go to the next loop iteration
+      }
 
       // Check for appointments and calendar events in this hour range
       const hasAppointment = await this.checkForAppointments(
@@ -73,10 +76,8 @@ export class AvailabilityService {
         startTime,
         endTime,
       );
-      console.warn({ hasAppointment });
 
       const hasCalendarEvent = await this.checkForEvents(startTime, endTime);
-      console.warn({ hasCalendarEvent });
 
       if (!hasAppointment && !hasCalendarEvent) {
         availableSlots.push({
@@ -85,70 +86,7 @@ export class AvailabilityService {
         });
       }
     }
-    console.warn({ availableSlots });
     return availableSlots;
-
-    // // If the start time of the day has passed, look for appointments from the current time
-    // if (currentStartDateTime < new Date()) {
-    //   const now = DateTime.now();
-    //   // Add an hour
-    //   const anHourLater = now.plus({ hours: 1 });
-
-    //   // Reset minutes and seconds at 0
-    //   const resetTime = anHourLater.set({
-    //     minute: 0,
-    //     second: 0,
-    //     millisecond: 0,
-    //   });
-    //   const ad30minLater = resetTime.plus({ minutes: 30 });
-
-    //   currentStartDateTime = ad30minLater;
-    // }
-
-    // // Search an appointment between the start and end of the selected day
-    // const appointments = await this.appointmentRepository.find({
-    //   where: {
-    //     staff: { id: staffId },
-    //     startDateAndTime: Between(currentStartDateTime, scheduleEndDateTime),
-    //   }, // Filter by date
-    //   order: { startDateAndTime: 'ASC' },
-    // });
-    // for (const appointment of appointments) {
-    //   // Appointment Start Time
-    //   const appointmentStartTime = new Date(appointment.startDateAndTime);
-
-    //   // If there is a space available before the appointment
-    //   if (appointmentStartTime > currentStartDateTime) {
-    //     const events = await this.calendarService.checkEventsInRange(
-    //       currentStartDateTime.toUTCString(),
-    //       appointmentStartTime.toUTCString(),
-    //     );
-    //     console.warn({ beforeAppointment: events });
-
-    //     // Add the available space to the list
-    //     availableSlots.push({
-    //       start: currentStartDateTime.toISOString(),
-    //       end: appointmentStartTime.toISOString(),
-    //     });
-    //   }
-    //   // Update the start time
-    //   currentStartDateTime = new Date(appointment.endDateAndTime);
-    // }
-
-    // // If there is a space available after the last appointment
-    // if (currentStartDateTime < scheduleEndDateTime) {
-    //   const events = await this.calendarService.checkEventsInRange(
-    //     currentStartDateTime.toUTCString(),
-    //     scheduleEndDateTime.toUTCString(),
-    //   );
-    //   console.warn({ afterAppointment: events });
-    //   availableSlots.push({
-    //     start: currentStartDateTime.toISOString(),
-    //     end: scheduleEndDateTime.toISOString(),
-    //   });
-    // }
-
-    // return availableSlots;
   }
 
   private async checkForAppointments(
@@ -156,14 +94,15 @@ export class AvailabilityService {
     startDate: Date,
     endDate: Date,
   ) {
-    const appointments = await this.appointmentRepository.find({
-      where: {
-        staff: { id: staffId },
-        startDateAndTime: Between(startDate, endDate),
-      },
-    });
+    const appointments =
+      (await this.appointmentRepository.count({
+        where: {
+          staff: { id: staffId },
+          startDateAndTime: Between(startDate, endDate),
+        },
+      })) > 0;
 
-    return appointments.length > 0; // True if there's at least one appointment
+    return appointments; // True if there's at least one appointment
   }
 
   private async checkForEvents(startTime: Date, endTime: Date) {
