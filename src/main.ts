a0c175a2b3de6de8396cfd4105c082fedcc122cc
Alcides Turruellas <ayunierto@gmail.com>
2025-10-29 23:45:32 -0500

    eliminar el uso de body-parser y su límite de carga en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +13,6 @@ async function bootstrap() {
   );
 
   app.enableCors();
-
-  app.use(bodyParser.json({ limit: '50mb' })); // Increase upload limit to 50mb
 
   await app.listen(process.env.PORT ?? 3000);
 }
