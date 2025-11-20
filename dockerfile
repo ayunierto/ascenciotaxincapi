8c251d6df55a794feb319acf685ca923a020a923
Alcides Turruellas <ayunierto@gmail.com>
2025-11-20 00:40:55 -0500

    feat: upgrade Docker config

---
+++ dockerfile
@@ -0,0 +1,22 @@
+FROM node:20-alpine3.21
+
+WORKDIR /usr/src/app
+
+# Copiar archivos de dependencias
+COPY package.json ./
+COPY package-lock.json ./
+
+# Instalar dependencias
+RUN npm install
+
+# Copiar el resto del proyecto
+COPY . .
+
+# # Generar cliente de Prisma
+# RUN npx prisma generate
+
+# Exponer el puerto
+EXPOSE 3000
+
+CMD ["sh", "-c", "npx prisma generate && npm run start:dev"]
+
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  const logger = new Logger('App - Auth');
+
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.enableCors({
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,8 +28,8 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
-
   await app.listen(process.env.PORT || 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
